f85247a3a94910dd44967d65ec7c1821dcf875e2
YashilDepani <51193749+YashilDepani@users.noreply.github.com>
2020-08-08 10:54:30 +0530

    fixed multiselect options for edit profile

--- AhdUni/account/models.py
+++ AhdUni/account/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from multiselectfield import MultiSelectField
 # Create your models here.
 
 DEFAULT_TIME = datetime(2020, 6, 13, 16, 0, 0)
@@ -141,10 +142,10 @@ class User_Details(models.Model):
     height          = models.IntegerField()
     current_weight  = models.IntegerField()
     set_goal        = models.CharField(max_length=200, choices=CATEGORY_SG)
-    workout_patterns= models.CharField(max_length=200,choices=CATEGORY_WP)
+    workout_patterns= MultiSelectField(choices=CATEGORY_WP)
     daily_water     = models.CharField(max_length=200, choices=CATEGORY_WI)
     #current_diet
-    reason          = models.CharField(max_length=200, choices=CATEGORY_REASON)
+    reason          = MultiSelectField(max_length=200, choices=CATEGORY_REASON)
     ongoing_med     = models.CharField(max_length=200, choices=CATEGORY_MED)
     ongoing_med_reason = models.CharField(max_length=200,null=True)
     menstural_cycle = models.CharField(max_length=200 , null=True)

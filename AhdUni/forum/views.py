de18afaf350d4cf5d440161a33e9cb7a74682e7f
vinay kakkad <imkkad@gmail.com>
2020-08-23 19:21:58 +0530

    started the frontend for forum

--- AhdUni/forum/views.py
+++ AhdUni/forum/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import View
 from .models import Post
 
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'forum/forum.html'
 
-class PostDetail(generic.DetailView):
-    model = Post
-    template_name = 'forum/post_detail.html'
+def post_list_view(request, *args, **kwargs):
+	post_list = Post.objects.all()
 
+	context = {'post_list': post_list}
+	return render(request, 'forum/forum.html', context)
 
+def post_detail_view(request, slug, *args, **kwargs):
+	post = Post.objects.get(slug=slug)
+
+	context = {'object': post}
+	return render(request, 'forum/post_detail.html', context)
